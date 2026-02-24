c0441fa77698ac0ff64b160bad78613252c72d8c
Naman Vaish <namanvaish374@gmail.com>
2026-02-24 21:53:26 +0530

    lecture 2 commit with operators assignment

---
+++ lec2.js
@@ -0,0 +1,38 @@
+
+var num="NAMAN";
+console.log(num);
+console.log(typeof(num));
+num=false;
+//function
+
+
+var sayhello=function(){
+    alert("Heloo world");
+}
+
+//array
+
+
+var arr=[1,2,3];
+var ani=new Array["DOG","CAT"];
+
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("False");
+}
+document.write("Starting Loop" +"<br/>");
+var count;
+for(count=0;count<10;count=count+1)
+{
+    document.write("current count  "+ count);
+}
+document.write("closing Loop" +"<br/>");
+
+
+
---
+++ operator.js
@@ -0,0 +1,47 @@
+//ARITHEMATIC 
+
+
+
+
+//Addition
+
+
+var a=10;
+var b=20;
+
+let result;
+ result=a+b;
+
+console.log(result);
+
+//Subtraction
+ 
+result=a-b;
+
+console.log(result);
+
+//MULTIPLICATION
+
+result=a*b;
+
+console.log(result);
+
+//DIVISIOn
+
+
+result=a/b;
+
+console.log(result);
+
+//Modulus
+
+result=a%b;
+
+console.log(result);
+
+
+//COMPARISON
+
+//double equals
+
+
