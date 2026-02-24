eaf5bc28f0671ca244e8f826253a49b7252dd159
Naman Vaish <namanvaish374@gmail.com>
2026-02-24 21:55:30 +0530

    lecture 1 java script

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// console.log("Hello World");
-// document.write("Hello World");
-
-// var num="NAMAN";
-// console.log(num);
-// console.log(typeof(num));
-// num=false;
-
-// var sayhello=function(){
-//     alert("Heloo world");
-// }
-// //array
-// var arr=[1,2,3];
-// var ani=new Array["DOG","CAT"];
-
-const sym1=Symbol(4);
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true");
-}
-else{
-    console.log("False");
-}
-document.write("Starting Loop" +"<br/>");
-var count;
-for(count=0;count<10;count=count+1)
-{
-    document.write("current count "+ count);
-}
-
-
+console.log("Hello World");
+document.write("Hello World");
 
