0c0c4f114f36280a18f31c22472a2bbfdb884394
Naman Vaish <namanvaish374@gmail.com>
2026-03-21 16:47:46 +0530

    my commit

---
+++ dom.js
@@ -0,0 +1,36 @@
+// <!-- 
+//  1. Java script can change all the html elements on the page 
+//  2. java script can change all the html attribute
+//  3.    ||       all the css style on the page
+//  4. Js can remove existing existing elements and attributes
+//  5. js can react to the all the existing java script events
+//  6.js can create a new html events in the page
+//   -->
+
+//  let x = document.getElementById("subjects"); 
+//  x.innerHTML="   ";
+
+//  let y= document.getElementsByClassName("subject");
+//  y[0].innerHTML=" ";
+//   let n= document.getElementsByTagName("subject");
+//   n[0].innerHTML=" ";
+
+
+  //the queryselector will return you the value at first indentified value
+  //the queryselectorall will return you the value at any index
+    
+
+ let x= document.getElementById("demo").style.color="blue";
+ 
+ // ----------------------------------------
+
+ let par=document.getElementsByTagName("body");
+
+ let y=document.createElement("h2");
+
+ par[0].appendChild(y);
+
+ y.remove();
+
+< button onclusive= "onclick ()">on mouse</button>
+
---
+++ switch.js
@@ -0,0 +1,22 @@
+// var grade='A';
+// document.write("Entering switch block <br/>");
+// switch(grade)
+// {
+//     case'A': 
+// }
+// function myFunction()
+// {
+//     alert("Hello World");
+// }
+
+
+
+// function mult(x,y)
+// {
+//     return x*y;
+
+// }
+let add = (x,y)=> {
+    console.log(x+y);
+}
+add(2,3);
